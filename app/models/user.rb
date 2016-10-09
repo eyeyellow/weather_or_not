79943be2da63d1d47567346770e4b89be2abc21a
Frank Aiello <frankvaiello@gmail.com>
2016-10-09 14:24:43 -0700

    Adds BCrypt in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+  include BCrypt
+
+  has_many :entries, class_name: "Entry"
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
