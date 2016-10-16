10d068313e89a9a051f1327b6e9f1708fdb2907f
Frank Aiello <frankvaiello@gmail.com>
2016-10-16 15:49:26 -0700

    Adds logic to create new User

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @user = User.find_by_email(user_params[:email])
-      if @user && @user.password == user_params[:password]
+    if @user = User.find_by_email(session_params[:email])
+      if @user && @user.password == session_params[:password]
         session[:user_id] = @user.id
         redirect_to @user
       else
@@ -25,7 +25,7 @@ class SessionsController < ApplicationController
 
   private
 
-  def user_params
+  def session_params
     params.require(:user).permit(:email, :password)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,32 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+    p "*" * 20
+    p params
+    p "*" * 20
+    if params[:user][:password] == params[:user][:confirm_password]
+      p "password confirmed"
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        p "user creation successful"
+        redirect_to @user
+      else
+        p "user creation was unsuccessful"
+      end
+    else
+      redirect_to root_url
+    end
+  end
+
   def show
     @user = User.find_by_id(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
