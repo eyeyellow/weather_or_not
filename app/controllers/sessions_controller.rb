20eeb1557332d61bb2f1f612a1ad3a1bcd21a8f4
Frank Aiello <frankvaiello@gmail.com>
2016-10-16 09:30:02 -0700

    Adds user authentication for sessions w/o strong params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,23 @@ class SessionsController < ApplicationController
   def create
     p "*" * 20
     p params
+    p "*" * 20
+    p params[:user][:email]
+    p User.find_by_email(params[:user][:email])
+    p "*" * 20
+    if @user = User.find_by_email(params[:user][:email])
+      if @user && @user.password == params[:user][:password]
+        session[:user_id] = @user.id
+        p "successful login"
+        p "session[:user_id] is #{session[:user_id]}"
+        redirect '/entries'
+      else
+        redirect '/'
+        p "no password match"
+      end
+    else
+      p "user not found"
+      redirect '/'
+    end
   end
 end
