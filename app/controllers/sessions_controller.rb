a01fe026112a4658468789ef2254bb213ae898ee
Frank Aiello <frankvaiello@gmail.com>
2016-10-16 15:13:11 -0700

    Adds logic to create and destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,32 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def show
-    p params
   end
 
   def create
-    p "*" * 20
-    p params
-    p "*" * 20
-    p params[:user][:email]
-    p User.find_by_email(params[:user][:email])
-    p "*" * 20
-    p user_params
-    p "*" * 20
     if @user = User.find_by_email(user_params[:email])
       if @user && @user.password == user_params[:password]
         session[:user_id] = @user.id
-        p "successful login"
-        p "session[:user_id] is #{session[:user_id]}"
         redirect_to @user
       else
-        redirect '/'
-        p "no password match"
+        redirect_to root_url
       end
     else
-      p "user not found"
-      redirect '/'
+      redirect_to root_url
     end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url
   end
 
   private
