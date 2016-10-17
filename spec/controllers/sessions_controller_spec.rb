4229f8c66f3f48cbf1321faa68aef8fe9c05f3df
Frank Aiello <frankvaiello@gmail.com>
2016-10-17 16:53:12 -0700

    Adds contoller specs for new session and new user

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
+  describe "GET sessions/new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the new session template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
 end
